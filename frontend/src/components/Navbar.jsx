bdffb0c9c0dac9d1f020fb4490415adb00b43f9b
ankitraj8042 <ankitrajj23@gmail.com>
2026-06-12 11:03:44 +0530

    test3

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -28,7 +28,7 @@ function Navbar() {
     <nav className="navbar">
       <div className="container navbar-content">
         <Link to="/" className="navbar-brand" onClick={closeMenu}>
-          🏥 HealthHub
+          🏥 HealthHub test2
         </Link>
 
         <div className="navbar-right">
